ca884a9331eff08bf2d4ef466c0db924e719774f
moshawa <moshawa0@gmail.com>
2022-11-11 00:45:18 -0500

    add gsap types

--- gsap.js
+++ gsap.js
@@ -1,3 +1,9 @@
 const sectionEls = document.querySelectorAll("section")
 
-gsap.from("main", { opacity: 0, duration: 0.5, x: -200, ease: "back" })
+gsap.from("section > *", {
+	opacity: 0,
+	duration: 0.5,
+	x: -200,
+	ease: "back",
+	stagger: 0.5,
+})
