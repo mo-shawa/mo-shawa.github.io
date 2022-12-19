3fe7f39dc68cb02d21b83d187b4fa3cad676a540
moshawa <moshawa0@gmail.com>
2022-12-19 18:47:32 -0500

    mostly fix projects, add backdrop filter

--- gsap.js
+++ gsap.js
@@ -1,61 +1,74 @@
-const sectionEls = document.querySelectorAll('section')
-const cardEls = document.querySelectorAll('.card')
+const sectionEls = document.querySelectorAll("section")
+const cardEls = gsap.utils.toArray(".card")
 
 gsap.registerPlugin(Flip, ScrollTrigger)
 
-gsap.from('#hero', {
+gsap.from("#hero", {
 	opacity: 0,
 	duration: 1.5,
 	x: -100,
-	ease: 'sine.inOut',
+	ease: "sine.inOut",
 	scrollTrigger: {
-		trigger: 'section',
-		start: 'top bottom',
-		toggleActions: 'play pause play pause',
+		trigger: "section",
+		start: "top bottom",
+		toggleActions: "play pause play pause",
 	},
 })
 
 cardEls.forEach((card, idx) => {
-	card.addEventListener('click', () => {
-		const state = Flip.getState('.card')
+	card.addEventListener("click", () => {
+		const state = Flip.getState(".card")
 
-		const isCardActive = card.classList.contains('active')
+		const isCardActive = card.classList.contains("active")
 
 		cardEls.forEach((innerCard) => {
-			innerCard.classList.remove('active')
-			innerCard.classList.add('inactive')
-			if (isCardActive) innerCard.classList.remove('inactive')
+			innerCard.classList.remove("active")
+			innerCard.classList.add("inactive")
+
+			if (isCardActive) innerCard.classList.remove("inactive")
 		})
 
 		if (!isCardActive) {
-			card.classList.remove('inactive')
-			card.classList.add('active')
+			card.classList.remove("inactive")
+			card.classList.add("active")
 		}
 
 		Flip.from(state, {
 			duration: 0.5,
-			ease: 'sine.inOut',
+			ease: "back.inOut",
 			absolute: true,
+			stagger: 0.1,
+			simple: true,
+			onComplete: () => {
+				// document.querySelector(".card.active").classList.remove("active")
+			},
 		})
+
+		if (!isCardActive) {
+			gsap.from(card.childNodes[1].childNodes, {
+				delay: 0.5,
+				opacity: 0,
+			})
+		}
 	})
 })
 
-gsap.from('#hero ~ div', {
+gsap.from("#hero ~ div", {
 	delay: 1.5,
 	opacity: 0,
 	y: 40,
 	duration: 1,
 })
 
-gsap.to('.point', {
+gsap.to(".point", {
 	duration: 1.5,
 	y: 20,
 	repeat: -1,
 	yoyo: true,
-	ease: 'sine.inOut',
+	ease: "sine.inOut",
 	scrollTrigger: {
-		trigger: '.point',
-		start: 'top bottom',
-		toggleActions: 'play pause play pause',
+		trigger: ".point",
+		start: "top bottom",
+		toggleActions: "play pause play pause",
 	},
 })
--- projects.js
+++ projects.js
@@ -1,58 +1,59 @@
 const projects = [
 	{
-		name: 'Ask GPT-3',
+		name: "Ask GPT-3",
 		description:
-			'Ask an advanced AI a question and get a response! Your questions and answers are saved locally in your browser, so you can come back to them later.',
-		technologies: ['GPT-3', 'React.js', 'React Spring', 'Node.js'],
-		github: 'https://github.com/mo-shawa/openAI-AMA',
-		deployment: 'http://openai-ama.herokuapp.com/',
-		image: 'project-img/OpenAI.webp',
+			"Ask an advanced AI a question and get a response! Your questions and answers are saved locally in your browser, so you can come back to them later.",
+		technologies: ["GPT-3", "React.js", "React Spring", "Node.js"],
+		github: "https://github.com/mo-shawa/openAI-AMA",
+		deployment: "http://openai-ama.herokuapp.com/",
+		image: "project-img/OpenAI.webp",
 	},
 	{
-		name: 'SaveQuest',
+		name: "SaveQuest",
 		description:
-			'SaveQuest gamifies the budgeting process, and encourages users to consistently track their expenses with EXP, levelling up, and a retro NES-style interface.',
-		technologies: ['React.js', 'Express', 'MongoDB', 'Node.js'],
-		github: 'https://github.com/mo-shawa/SaveQuest',
-		deployment: 'http://savequest.herokuapp.com/',
-		image: 'project-img/SaveQuest_scaled.webp',
+			"SaveQuest gamifies the budgeting process, and encourages users to consistently track their expenses with EXP, levelling up, and a retro NES-style interface.",
+		technologies: ["React.js", "Express", "MongoDB", "Node.js"],
+		github: "https://github.com/mo-shawa/SaveQuest",
+		deployment: "http://savequest.herokuapp.com/",
+		image: "project-img/SaveQuest_scaled.webp",
 	},
 	{
-		name: 'HelloFren',
+		name: "HelloFren",
 		description:
 			"HelloFren is a social media platform that is designed to put your furry friends in the spotlight. Add your pet(s) to your profile and share with the world what they're up to!",
-		technologies: ['Python', 'Django', 'Bootstrap', 'PostgreSQL'],
-		github: 'https://github.com/mo-shawa/spot',
-		deployment: 'http://hellofren.herokuapp.com/',
-		image: 'project-img/hellofren_scaled.webp',
+		technologies: ["Python", "Django", "Bootstrap", "PostgreSQL"],
+		github: "https://github.com/mo-shawa/spot",
+		deployment: "http://hellofren.herokuapp.com/",
+		image: "project-img/hellofren_scaled.webp",
 	},
 	{
-		name: 'Hall of Game',
+		name: "Hall of Game",
 		description:
-			'Collect, curate, share, and discuss with others your favorite characters, the most memorable items, and iconic maps from the games we all loved playing.',
-		technologies: ['EJS', 'Express.js', 'MongoDB', 'Bootstrap'],
-		github: 'https://github.com/mo-shawa/hall-of-game',
-		deployment: 'http://hallofgame.herokuapp.com/',
-		image: 'project-img/HoG_scaled.webp',
+			"Collect, curate, share, and discuss with others your favorite characters, the most memorable items, and iconic maps from the games we all loved playing.",
+		technologies: ["EJS", "Express.js", "MongoDB", "Bootstrap"],
+		github: "https://github.com/mo-shawa/hall-of-game",
+		deployment: "http://hallofgame.herokuapp.com/",
+		image: "project-img/HoG_scaled.webp",
 	},
 ]
 
 for (let project of projects) {
-	const card = document.createElement('div')
-	card.classList = 'card'
+	const card = document.createElement("div")
+	card.classList = "card shadow"
 
-	const h2 = document.createElement('h2')
+	const h2 = document.createElement("h2")
 	h2.textContent = project.name
 
-	const img = document.createElement('img')
+	const img = document.createElement("img")
 	img.src = project.image
+	img.alt = project.name
 
-	const pEl = document.createElement('p')
+	const pEl = document.createElement("p")
 	pEl.textContent = project.description
 
-	const innerDiv = document.createElement('div')
+	const innerDiv = document.createElement("div")
 	innerDiv.append(h2, pEl)
 
 	card.append(img, innerDiv)
-	document.getElementById('projects').appendChild(card)
+	document.getElementById("projects").appendChild(card)
 }
