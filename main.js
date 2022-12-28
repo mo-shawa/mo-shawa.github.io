afcfa28fa25e7753d3d06c8e3a823044d5a0ee16
moshawa <moshawa0@gmail.com>
2022-12-28 01:24:21 -0500

    cleanup

--- main.js
+++ main.js
@@ -26,7 +26,9 @@ let initialMove = false
 document.addEventListener("mousemove", (evt) => {
 	if (!initialMove) {
 		document.body.style.color = "var(--light-text-color)"
+		console.log("color changed")
 		document.querySelector("nav").style.visibility = "visible"
+
 		initialMove = true
 	}
 
