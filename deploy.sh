3fbf437daa5a4c523032ba9ceb02f2ef5714aa84
moshawa <moshawa0@gmail.com>
2022-12-28 04:15:25 -0500

    fix 404s attempt

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:mo-shawa/mo-shawa.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:mo-shawa/mo-shawa.git main:gh-pages
+# git push -f git@github.com:mo-shawa/mo-shawa.git main:gh-pages
 
 cd -
