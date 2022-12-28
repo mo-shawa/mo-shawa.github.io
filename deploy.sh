dbf72df1711ebaf6a1691fae4d04b263753c6a21
moshawa <moshawa0@gmail.com>
2022-12-28 18:36:14 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,22 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
+# echo > .nojekyll
+echo > dist/.nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.shawa.dev' > CNAME
 
-git init
-git checkout -B main
+# git init
+# git checkout -B main
 git add -A
 git commit --allow-empty -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:mo-shawa/mo-shawa.github.io.git gh-pages
+git push -f origin gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:mo-shawa/mo-shawa.git main:gh-pages
