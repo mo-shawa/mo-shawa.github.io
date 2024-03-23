3bf419222487e8e27a28544124f2206b4adbd3c4
moshawa <moshawa0@gmail.com>
2024-03-23 14:50:46 -0400

    potential fix for safari

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home() {
         ref={scrollContainerRef}
         className="h-[300vh] overflow-x-clip bg-gradient-to-b from-pink-100 via-violet-100 to-white  px-4"
       >
-        <motion.div className="sticky top-0  mx-auto mb-4 grid h-screen max-w-7xl grid-cols-1 items-center gap-4 md:grid-cols-[2fr_1fr]">
+        <motion.div className="sticky left-0 top-0 mx-auto mb-4 grid h-screen max-w-7xl grid-cols-1 items-center gap-4 md:grid-cols-[2fr_1fr]">
           <TextBubbles scrollYProgress={scrollYProgress} />
           <motion.div
             initial={{
